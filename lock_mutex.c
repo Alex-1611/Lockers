f60f05273e10800e91c994905333f9ca8b4e4aa5
Alex-1611 <alex_timplarescu@yahoo.com>
2025-01-16 00:49:08 +0200

    Mutex & Semaphore

---
+++ lock_mutex.c
@@ -0,0 +1,186 @@
+#include "lock_mutex.h"
+
+thr_queue* thr_init_queue(){
+    thr_queue *queue = (thr_queue*)malloc(sizeof(thr_queue));
+    queue->front = NULL;
+    queue->rear = NULL;
+    return queue;
+}
+
+bool is_empty(thr_queue *queue){
+    if(queue->front == NULL)
+        return true;
+    return false;
+}
+
+void enqueue(thr_queue *queue, pid_t tid){
+    if(!is_empty(queue))
+    {   
+        thr_node *node = (thr_node*)malloc(sizeof(thr_node));
+        node->next = NULL;
+        node->tid = tid;
+        queue->rear->next = node;
+        queue->rear = node;
+    }
+    else
+    {
+        thr_node *node = (thr_node*)malloc(sizeof(thr_node));
+        node->next = NULL;
+        node->tid = tid;
+        queue->front = node;
+        queue->rear = node;
+    }
+}
+
+pid_t dequeue(thr_queue *queue){
+    if(!is_empty(queue)){
+        thr_node *node = queue->front;
+        pid_t tid = queue->front->tid;
+        queue->front = queue->front->next;
+        if(queue->front == NULL)
+            queue->rear = NULL;
+        free(node);
+        return tid;
+    } 
+    else
+        return 0;
+}
+
+void delete_tid(thr_queue *queue, pid_t tid){
+    if(!is_empty(queue)){
+        thr_node *node = queue->front;
+        thr_node *prev = NULL;
+        while(node != NULL)
+        {
+            if(node->tid == tid)
+            {
+                if(prev == NULL)
+                    queue->front = node->next;
+                else
+                    prev->next = node->next;
+                free(node);
+                return;
+            }
+            prev = node;
+            node = node->next;
+        }
+    }
+}
+
+bool in_queue(thr_queue *queue, pid_t tid){
+    if(is_empty(queue))
+        return false;
+    else
+    {
+        thr_node *node = queue->front;
+        while(node != NULL)
+        {
+            if(node->tid == tid)
+                return true;
+            else
+                node = node->next;
+        }
+        return false;
+    }
+}
+
+bool in_first_n_queue(semaphore *sem, pid_t tid){
+    if(is_empty(sem->thr_queue))
+        return false;
+    else
+    {
+        thr_node *node = sem->thr_queue->front;
+        int i = 0;
+        while(node != NULL && i < sem->value)
+        {
+            if(node->tid == tid)
+                return true;
+            else
+            {
+                node = node->next;
+                i++;
+            }
+        }
+        return false;
+    }
+}   
+
+pid_t nth_in_queue(semaphore *sem){
+    if(is_empty(sem->thr_queue))
+        return 0;
+    else
+    {
+        thr_node *node = sem->thr_queue->front;
+        int i = 0;
+        while(node != NULL && i < sem->value-1)
+        {
+            node = node->next;
+            i++;
+        }
+        if(node != NULL && i == sem->value)
+            return node->tid;
+        else
+            return 0;
+    }
+}
+
+mtx_lock *mtx_init(){
+    mtx_lock *mtx = (mtx_lock*)malloc(sizeof(mtx_lock));
+    mtx->thr_queue = thr_init_queue();
+    return mtx;
+}
+
+void mtx_destroy(mtx_lock *mtx){
+    while((!is_empty(mtx->thr_queue))){
+        pthread_kill((mtx->thr_queue->front->tid), SIGKILL);
+        dequeue(mtx->thr_queue);
+    }
+    free(mtx->thr_queue);
+    free(mtx);
+}
+
+void acquire(mtx_lock *mtx){
+    pid_t tid = gettid();
+    if(!in_queue(mtx->thr_queue, tid))
+        enqueue(mtx->thr_queue, tid);
+    while(mtx->thr_queue->front->tid != tid)
+        pause();
+}
+
+void release(mtx_lock *mtx){
+    pid_t tid = gettid();
+    if(!is_empty(mtx->thr_queue) && mtx->thr_queue->front->tid == tid)
+        dequeue(mtx->thr_queue);
+    pthread_kill(mtx->thr_queue->front->tid, SIGCONT);
+}
+
+semaphore *sem_init(int value){
+    semaphore *sem = (semaphore*)malloc(sizeof(semaphore));
+    sem->value = value;
+    sem->thr_queue = thr_init_queue();
+    return sem;
+}
+
+void wait(semaphore *sem){
+    pid_t tid = gettid();
+    if(!in_queue(sem->thr_queue, tid))
+        enqueue(sem->thr_queue, tid);
+    while(!in_first_n_queue(sem, tid))
+        sleep(0.1);
+}
+
+void signal(semaphore *sem){
+    pid_t tid = gettid();
+    delete_tid(sem->thr_queue, tid);
+    if(nth_in_queue(sem))
+        pthread_kill(nth_in_queue(sem), SIGCONT);
+}
+
+void sem_destroy(semaphore *sem){
+    while((!is_empty(sem->thr_queue))){
+        pthread_kill((sem->thr_queue->front->tid), SIGKILL);
+        dequeue(sem->thr_queue);
+    }
+    free(sem->thr_queue);
+    free(sem);
+}
---
+++ lock_mutex.h
@@ -0,0 +1,43 @@
+#include <unistd.h>
+#include <pthread.h>
+#include <stdbool.h>
+#include <stdatomic.h>
+#include <signal.h>
+
+
+typedef struct thr_node{
+    pid_t tid;
+    struct thr_node *next;
+}thr_node;
+
+typedef struct thr_queue{
+    thr_node *front;
+    thr_node *rear;
+}thr_queue;
+
+thr_queue *thr_init_queue();
+bool is_empty(thr_queue *queue);
+void enqueue(thr_queue *queue, pid_t tid);
+pid_t dequeue(thr_queue *queue);
+bool in_queue(thr_queue *queue, pid_t tid);
+void delete_tid(thr_queue *queue, pid_t tid);
+
+
+typedef struct mtx_lock{
+    thr_queue *thr_queue; 
+}mtx_lock;
+
+mtx_lock *mtx_init();
+void mtx_destroy(mtx_lock *mtx);
+
+void acquire(mtx_lock *mtx);
+void release(mtx_lock *mtx);
+
+typedef struct semaphore{
+    int value;
+    thr_queue *thr_queue;
+}semaphore;
+
+pid_t nth_in_queue(semaphore *sem);
+bool in_first_n_queue(semaphore *sem, pid_t tid);
+
